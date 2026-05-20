56552d71fb8179cfb85e9eac402df5fd0420a1fd
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2026-05-20 08:51:05 +0200

    Update document configuration for waRestInsluit

--- WARestInsluit/js/document-config.js
+++ WARestInsluit/js/document-config.js
@@ -1,13 +1,13 @@
 var documentConfig =
 {
   title: "Gebruik activiteiten Restcategorie en Andere insluitsystemen",
-  shortName: "restcat-insluit",
+  shortName: "waRestInsluit",
   pubDomain: "imev",
   specStatus: "def",
   specType: "wa",
   license: "cc-by-nd",
   latestVersion: [
-    "https://docs.geostandaarden.nl/imev/werkafspraken/"
+    "https://docs.geostandaarden.nl/imev/waRestInsluit/"
   ],
   publishDate: "2026-05-19",
   thisVersion: [
@@ -16,7 +16,7 @@ var documentConfig =
   github: "https://github.com/geonovum/imev-werkafspraken/",
   issueBase: "https://github.com/geonovum/imev-werkafspraken/issues/",
   edDraftURI: "https://geonovum.github.io/imev-werkafspraken/",
-  canonicalURI: "https://docs.geostandaarden.nl/imev/werkafspraken/",
+  canonicalURI: "https://docs.geostandaarden.nl/imev/waRestInsluit/",
   authors: [
     {
       name: "Monique van Scherpenzeel",
