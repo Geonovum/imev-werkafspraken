979e6136bbf1aeacfb2b7e8a273414da26f62cd5
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2026-05-21 10:57:23 +0200

    Comment out previousMaturity property
    Comment out the previousMaturity property in document-config.js

--- WARestInsluit/js/document-config.js
+++ WARestInsluit/js/document-config.js
@@ -14,7 +14,7 @@ var documentConfig =
     "https://geonovum.github.io/imev-werkafspraken/"
   ],
   previousPublishDate: [],
-  previousMaturity: "CV",
+  // previousMaturity: "CV",
   github: "https://github.com/geonovum/imev-werkafspraken/",
   issueBase: "https://github.com/geonovum/imev-werkafspraken/issues/",
   edDraftURI: "https://geonovum.github.io/imev-werkafspraken/",
